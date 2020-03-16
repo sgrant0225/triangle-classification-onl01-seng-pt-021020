65b55541bc2987d40a859389d4372c0be76a457f
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-16 16:08:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
      begin
     raise TriangleError
-  
+  end
   elsif  s1 == s2 && s1 == s3
    self.kind = :equilateral
   elsif s1 == s2 || s1 == s3 || s2 == s3 
