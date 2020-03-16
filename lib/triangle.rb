e3327ae7dd76e016e2393be7e1412506d7a0d671
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-16 16:25:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
-  # write code here
+   attr_accessor :s1, :s2, :s3
+    
+  def initialize(s1, s2, s3)
+   @s1 = s1
+   @s2 = s2
+   @s3 = s3
+  end
+  
+  def kind 
+   #if any of these conditions below return false 
+   if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+     begin
+     raise TriangleError
+    end
+  
+  elsif  s1 == s2 && s1 == s3
+   :equilateral
+  elsif s1 == s2 || s1 == s3 || s2 == s3 
+    :isosceles
+  else
+    :scalene
+   end
+  end
+
+class TriangleError < StandardError
+  def message 
+    "One side cannot exceed any two sides combined and all sides must be larger than zero!"
+  end
+ end
 end
