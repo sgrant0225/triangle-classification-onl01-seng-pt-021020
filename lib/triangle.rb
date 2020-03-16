a2981ebf0a7b00de5d08d660083628c3771ff74c
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-16 15:37:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,13 @@ class Triangle
    attr_accessor :side_1, :side_2, :side_3
     
   def initialize(side_1, side_2, side_3)
-   @side_1 = side_2
-   @side_2 = side_2
-   @side_3 = side_3
+   @side_1 = s1
+   @side_2 = s2
+   @side_3 = s3
   end
   
   def kind 
    #if any of these conditions below return false 
-   
-  end
+   if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1 == s2
+  
 end
